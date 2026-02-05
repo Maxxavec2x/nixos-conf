04953bd5e513dfbb8ded2bead112944d397c79ca
maxxavec2x <maxencesibonibergard@gmail.com>
2026-02-05 17:39:17 +0100

    correction zsh completion

--- user-packages/zsh.nix
+++ user-packages/zsh.nix
@@ -71,7 +71,7 @@
       # -----------------------------
       # Completion: case-insensitive
       # -----------------------------
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+      zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
     '';
   };
 }
