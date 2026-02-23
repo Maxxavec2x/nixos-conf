ab12e8230a8fa86f2ffed42ed9405df9c215e1a3
maxxavec2x <maxencesibonibergard@gmail.com>
2026-02-23 18:42:53 +0100

    ajout virtualisation

--- configuration.nix
+++ configuration.nix
@@ -65,7 +65,7 @@ in
 
    users.users.maxx = {
      isNormalUser = true;
-     extraGroups = [ "wheel" "video" "input" "seat" ]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "wheel" "video" "input" "seat" "libvirtd" ]; # Enable ‘sudo’ for the user.
      shell = pkgs.zsh;
    };
 
