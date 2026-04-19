d3593513c25f01096fe76c62b8901617ffbef8e5
maxxavec2x <maxencesibonibergard@gmail.com>
2026-04-19 10:20:13 +0200

    ajout d'une liste d'exclusion pour le script wofi audio

--- user-packages/wofi-audio.nix
+++ user-packages/wofi-audio.nix
@@ -8,7 +8,10 @@
 let
   wofi-audio = pkgs.writeShellScriptBin "wofi-audio" ''
 #!/usr/bin/env bash
-
+# Ce script permet d'afficher les outputs et inputs en haut a droite de l'écran, dans une fenetre wofi.
+# On peut trier ce qu'on veut voir en précisant "input" ou "output" en argument.
+# On peut également exclude des noms avec la variable "EXCLUDE_NAMES"
+#
     set -e
     wofi_cmd=(wofi --show dmenu --prompt --hide-scroll --allow-markup --define=hide_search=true --location=top_right --width=600 --height=200 --xoffset=-60)
 
@@ -22,25 +25,36 @@ let
       esac
     }
 
-    pick() { # $1 = "Sinks" or "Sources"
-      wpctl status | awk -v want="$1" '
-              function strip_vol(s) { sub(/[[:space:]]*\[.*/, "", s); return s }
+# Noms à exclure
+#
+    EXCLUDE_NAMES=(
+      "Navi 31 HDMI/DP Audio Stéréo numérique (HDMI)"
+      "USB Audio Front Headphones"
+      "USB Audio S/PDIF Output"
+    )
 
+    pick() { # $1 = "Sinks" or "Sources"
+      local excl_arr
+      excl_arr=$(printf '%s\n' "''${EXCLUDE_NAMES[@]}")
+
+      wpctl status | awk -v want="$1" -v exclude="$excl_arr" '
+              BEGIN {
+                n = split(exclude, ex, "\n")
+                for (i=1; i<=n; i++) excl[ex[i]] = 1
+              }
+              function strip_vol(s) { sub(/[[:space:]]*\[.*/, "", s); return s }
               /^Audio$/                { audio=1; next }
               /^(Video|Settings)$/     { audio=0; inner=0; next }
-
               audio && $0 ~ /[[:space:]]Sinks:*$/   { inner=(want=="Sinks");  next }
               audio && $0 ~ /[[:space:]]Sources:*$/ { inner=(want=="Sources"); next }
-
               inner && ($0 ~ /[[:space:]]Filters:$/  || \
                     $0 ~ /[[:space:]]Streams:$/  || \
                     $0 ~ /^[[:space:]]*$/)                   { inner=0; next }
-
               inner && match($0, /^[^0-9]*([0-9]+)\.\s*(\*?)\s*(.*)$/, m) {
                 id=m[1]; star=(m[2]=="*" ? "★ " : ""); name=strip_vol(m[3])
-                printf "%s\t%s%s\n", id, star, name
+                if (!excl[name]) printf "%s\t%s%s\n", id, star, name
               }
-            ' | "''${wofi_cmd[@]}" "$1" | cut -f1
+            ' | sort -t$'\t' -k1,1n | "''${wofi_cmd[@]}" "$1" | cut -f1
     }
 
     if ! command -v wpctl >/dev/null 2>&1; then
