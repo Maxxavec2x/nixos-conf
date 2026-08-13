8cb8c630df3689420cddb110b963158f7b3761a2
maxxavec2x <maxencesibonibergard@gmail.com>
2026-08-13 23:20:31 +0100

    update conf nvim

--- modules/home/programming/nixvim.nix
+++ modules/home/programming/nixvim.nix
@@ -8,9 +8,12 @@
         inputs.nixvim.homeModules.nixvim
       ];
       programs.nixvim = {
+        nixpkgs.source = inputs.nixpkgs;
         enable = true;
         defaultEditor = true;
-
+        diagnostic.settings = {
+          virtual_text = true;
+        };
         # Treesitter : parsing/highlighting
         plugins.treesitter = {
           enable = true;
@@ -38,6 +41,25 @@
             format_on_save = {
               timeout_ms = 500;
               lsp_format = "fallback";
+            };
+          };
+        };
+
+        # Pour la complétion
+        plugins.blink-cmp = {
+          enable = true;
+          settings = {
+            keymap.preset = "super-tab"; # Tab/Ctrl-y pour valider
+            appearance = {
+              nerd_font_variant = "mono";
+            };
+            sources = {
+              default = [
+                "lsp"
+                "path"
+                "snippets"
+                "buffer"
+              ];
             };
           };
         };
