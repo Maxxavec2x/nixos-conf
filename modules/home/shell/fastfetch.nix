0639e74d1a4502f54bd72bdabc58ed47761feddd
maxxavec2x <maxencesibonibergard@gmail.com>
2026-08-19 09:59:13 +0100

    update fastfetch

--- modules/home/shell/fastfetch.nix
+++ modules/home/shell/fastfetch.nix
@@ -1,4 +1,6 @@
 # Conf fastfetch
+# POUR LA LISTE DES MODULES :
+# https://github.com/fastfetch-cli/fastfetch-wiki/blob/master/Json-Schema.md#511-property-json-config--modules--modules-items--anyof--item-0
 { inputs, ... }:
 {
   flake.homeModules.fastfetch =
@@ -17,6 +19,12 @@
           modules = [
             "title"
             "separator"
+            "os"
+            {
+              type = "command";
+              key = "OS Age";
+              text = "birth_install=$(stat -c %W /); current=$(date +%s); days_difference=$(( (current - birth_install) / 86400 )); echo $days_difference days";
+            }
             {
               type = "kernel";
               key = "Kernel";
@@ -36,6 +44,11 @@
             {
               type = "cpu";
               key = "CPU";
+            }
+            {
+              type = "gpu";
+              key = "GPU";
+              format = "{name}";
             }
             {
               type = "memory";
